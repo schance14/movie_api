a924f2a77cb5d49650aa2742501f936533b26831
schance14 <schance.sd14@gmail.com>
2023-09-18 12:15:20 -0600

    Created express code for all URL endpoints required

--- index.js
+++ index.js
@@ -1,58 +1,305 @@
 const express = require('express'),
-    morgan = require('morgan');
+    app = express(),
+    bodyParser = require('body-parser'),
+    uuid = require('uuid');
 
-const app = express();
+    app.use(bodyParser.json());
 
-app.use(morgan('common'));
+let users = [
+    {
+        id: 1,
+        name: "Lenny",
+        favoriteMovies: ["Mean Girls"]
+    },
+    {
+        id: 2,
+        name: "Patrick",
+        favoriteMovies: ["Lord of the Rings"]
+    },
+]
 
 
-let topMovies = [
+let movies = [
     {
-        title: 'The Twilight Saga'
+        "title": "The Twilight Saga",
+        "description": "",
+        "genre": {
+            "name":"",
+            "description":""
+        },
+        "director": {
+            "name":"",
+            "description":"",
+            "birth":""
+
+        },
+        
+        "imageUrl":""
     },
     {
-        title: 'Lord of the Rings'
+        title: "Lord of the Rings",
+        "description": "",
+        "genre": {
+            "name":"",
+            "description":""
+        },
+        "director": {
+            "name":"",
+            "description":"",
+            "birth":""
+
+        },
+        
+        "imageUrl":""
     },
     {
-        title: 'Harry Potter and the Prisoner of Azkaban'
+        title: "Harry Potter and the Prisoner of Azkaban",
+        "description": "",
+        "genre": {
+            "name":"",
+            "description":""
+        },
+        "director": {
+            "name":"",
+            "description":"",
+            "birth":""
+
+        },
+        
+        "imageUrl":""
     },
     {
-        title: 'Team America: World Police'
+        title: "Team America: World Police",
+        "description": "",
+        "genre": {
+            "name":"",
+            "description":""
+        },
+        "director": {
+            "name":"",
+            "description":"",
+            "birth":""
+
+        },
+        
+        "imageUrl":""
     },
     {
-        title: 'Tropic Thunder'
+        title: "Tropic Thunder",
+        "description": "",
+        "genre": {
+            "name":"",
+            "description":""
+        },
+        "director": {
+            "name":"",
+            "description":"",
+            "birth":""
+
+        },
+        
+        "imageUrl":""
     },
     {
-        title: 'The Great Gatsby'
+        title: "The Great Gatsby",
+        "description": "",
+        "genre": {
+            "name":"",
+            "description":""
+        },
+        "director": {
+            "name":"",
+            "description":"",
+            "birth":""
+
+        },
+        
+        "imageUrl":""
     },
     {
-        title: 'Inglorious Basterds'
+        title: "Inglorious Basterds",
+        "description": "",
+        "genre": {
+            "name":"",
+            "description":""
+        },
+        "director": {
+            "name":"",
+            "description":"",
+            "birth":""
+
+        },
+        
+        "imageUrl":""
     },
     {
-        title: 'Mean Girls'
+        title: "Mean Girls",
+        "description": "",
+        "genre": {
+            "name":"",
+            "description":""
+        },
+        "director": {
+            "name":"",
+            "description":"",
+            "birth":""
+
+        },
+        
+        "imageUrl":""
     },
     {
-        title: '21 Jump Street'
+        title: "21 Jump Street",
+        "description": "",
+        "genre": {
+            "name":"",
+            "description":""
+        },
+        "director": {
+            "name":"",
+            "description":"",
+            "birth":""
+
+        },
+        
+        "imageUrl":""
     },
     {
-        title: 'Scream'
+        title: "Scream",
+        "description": "",
+        "genre": {
+            "name":"",
+            "description":""
+        },
+        "director": {
+            "name":"",
+            "description":"",
+            "birth":""
+
+        },
+        
+        "imageUrl":""
     }
 ]; 
 
-app.get('/', (req, res)=> {
-    res.send('Welcome to Movie Flix!');
-});
 
-app.get('/movies', (req, res)=> {
-    res.json(topMovies);
-});
+//CREATE
+app.post('/users', (req, res)=> {
+    const newUser = req.body;
 
-app.use('/documentation.html', express.static('public'));
+    if (newUser.name) {
+        newUser.id = uuid.v4();
+        users.push.apply(newUser);
+        res.status(201).json(newUser)
+    } else {
+        res.status(400).send('user needs names')
+    }
+})
 
-app.use ((err, req, res)=> {
-    console.error(err.stack);
-    res.status(500).send('There has been an error!');
-});
+//UPDATE
+app.put('/users/:id', (req, res)=> {
+    const { id } = req.params;
+    const updatedUser = req.body;
+
+    let user = users.find( user => user.id == id );
+
+    if (user){
+        user.name = updatedUser.name;
+        res.status(200).json(user);
+    } else {
+        res.status(400).send('no such user')
+    }
+})
+
+//CREATE
+app.post('/users/:id/:movieTitle', (req, res)=> {
+    const { id, movieTitle } = req.params;
+
+    let user = users.find( user => user.id == id );
+
+    if (user){
+        user.favoriteMovies.push(movieTitle);
+        res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);;
+    } else {
+        res.status(400).send('no such user')
+    }
+})
+
+//DELETE
+app.delete('/users/:id/:movieTitle', (req, res)=> {
+    const { id, movieTitle } = req.params;
+
+    let user = users.find( user => user.id == id );
+
+    if (user){
+        user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
+        res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);;
+    } else {
+        res.status(400).send('no such user')
+    }
+})
+
+
+//DELETE
+app.delete('/users/:id', (req, res)=> {
+    const { id } = req.params;
+
+    let user = users.find( user => user.id == id );
+
+    if (user){
+        users = users.filter( user => user.id != id);
+        res.status(200).send(`User ${id} has been deleted`);;
+    } else {
+        res.status(400).send('no such user found')
+    }
+})
+
+
+//READ
+app.get('/movies', (req,res)=> {
+    res.status(200).json(movies);
+})
+
+//READ
+app.get('/movies/:title', (req,res)=> {
+    const { title } = req.params;
+    const movie = movies.find( movie => movie.title === title);
+
+    if (movie) {
+        res.status(200).json(movie);
+    } else {
+        res.status(400).send('not available')
+    }
+
+})
+
+//READ
+app.get('/movies/genre/:genreName', (req,res)=> {
+    const { genreName } = req.params;
+    const movie = movies.find( movie => movie.genre.name === genreName).genre;
+
+    if (movie) {
+        res.status(200).json(genre);
+    } else {
+        res.status(400).send('not available')
+    }
+    
+})
+
+
+//READ
+app.get('/movies/directors/:directorName', (req,res)=> {
+    const { directorName } = req.params;
+    const director = movies.find( movie => movie.director.name === directorName).director;
+
+    if (director) {
+        res.status(200).json(director);
+    } else {
+        res.status(400).send('not available')
+    }
+    
+})
+
 
 
 app.listen(8080, ()=> {
