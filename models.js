33aee89d4761d90406443221aad687095d629279
schance14 <schance.sd14@gmail.com>
2023-10-03 22:33:57 -0600

    Updated model syntax

--- models.js
+++ models.js
@@ -16,7 +16,7 @@ let movieSchema = mongoose.Schema({
 });
 
 let userSchema = mongoose.Schema({
-    Username: {type: String, required: true},
+    Name: {type: String, required: true},
     Password: {type: String, required: true},
     Email: {type: String, required: true},
     Birthday: Date,
