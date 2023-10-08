a1cfc527730a936afa835b6a553db71855fe1456
schance14 <schance.sd14@gmail.com>
2023-10-08 17:58:00 -0600

    created passport.js file
    configuration for local and JWT authentication

---
+++ passport.js
@@ -0,0 +1,48 @@
+const passport = require("passport");
+LocalStrategy = require('passport-local').Strategy,
+Models= require('./models.js'),
+passportJWT = require('passport-jwt');
+
+let Users = Models.User,
+JWTStrategy = passportJWT.Strategy,
+ExtractJWT= passportJWT.ExtractJwt;
+
+passport.use(
+    new LocalStrategy(
+        {
+            usernameField: 'Email',
+            passwordField: 'Password',
+        },
+        async (email, password, callback) => {
+            console.log(`${email} ${password}`);
+            await Users.findOne({Email: email})
+            .then((user)=> {
+                if (!user){
+                    console.log('incorrect email');
+                    return callback(null, false, { message: 'Incorrect email or password.',
+                });
+                }
+                console.log('finished');
+                return callback(null,user);
+            })
+            .catch((error)=> {
+                if (error) {
+                    console.log(error);
+                }
+            })
+        }
+    )
+);
+
+passport.use( new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'your_jwt_secret'
+}, async (jwtPayload, callback) => {
+    return await Users.findById(jwtPayload._id)
+    .then((user)=> {
+        return callback(null, user);
+    })
+    .catch((error)=> {
+        return callback(error)
+    });
+}));
