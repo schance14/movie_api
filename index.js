7c284bdd19b1d184fd0b52313021f2f632a8b7fa
schance14 <schance.sd14@gmail.com>
2023-10-23 14:40:04 -0600

    updated syntax

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Users = Models.User;
 
 //mongoose.connect('mongodb://127.0.0.1:27017/cfDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require('express'),
     app = express(),
