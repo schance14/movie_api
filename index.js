89f18cc6c60186c8fb12d279ba5ca99429d16eae
schance14 <schance.sd14@gmail.com>
2024-02-21 11:07:40 -0700

    refactor: syntax error for update user

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ app.put(
         if (!user) {
           return res.status(404).send("Error: User does not exsist");
         } else {
-          res.json(Updateduser);
+          res.json(updateduser);
         }
       })
       .catch((err) => {
