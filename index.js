796f0dca7233b7c9aaaa5d523b4a40e80a89a37b
schance14 <schance.sd14@gmail.com>
2024-02-23 18:24:22 -0700

    refactor: update CORS for Netlify

--- index.js
+++ index.js
@@ -21,7 +21,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require("cors");
-app.use(cors());
+let allowedOrigins = [
+  "https://film-finders.netlify.app/",
+  "http://localhost:1234/",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 let auth = require("./auth")(app);
 
